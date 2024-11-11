d4160722a4a8b1a2a0c5b88986d2ac458ee127f3
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-11 17:35:42 +0100

    add tag footer in AppFooter.jsx

--- react-task-list/src/components/AppFooter.jsx
+++ react-task-list/src/components/AppFooter.jsx
@@ -1,6 +1,6 @@
 //creo il componente AppFooter
 export default function AppFooter() {
     return (
-        <p>&copy; {new Date().getFullYear()} All rights reserved</p>
+        <footer><p>&copy; {new Date().getFullYear()} All rights reserved</p></footer>
     )
 }
