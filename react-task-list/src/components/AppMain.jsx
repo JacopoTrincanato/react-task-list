5d45c4c2a2140c76459a80a200e1ef2bffa1114e
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-11 15:42:05 +0100

    create AppMain.jsx and AppMain function and export it

---
+++ react-task-list/src/components/AppMain.jsx
@@ -0,0 +1,13 @@
+//creo la funzione AppMain
+export default function AppMain() {
+
+    //creo una variabile che stampi welcome to my react app
+    const welcomeMessage = 'welcome to my react app';
+
+    //eseguo il return del main
+    return (
+        <main>
+            <h2>{welcomeMessage}</h2>
+        </main>
+    )
+};
